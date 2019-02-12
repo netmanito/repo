4f42e77c1768b7a9c8314f72ef21a30ec1d5fc16
jacinto calvo <jaci@seriousman.org>
2019-02-12 16:07:45 +0100

    updated bash-me

--- bash-me.sh
+++ bash-me.sh
@@ -16,13 +16,15 @@ if [ -z "$ALIASES" ]; then
 else
         echo "Downloading bash aliases extra"
         curl -O https://raw.githubusercontent.com/netmanito/repo/master/bash-aliases-extra
+        curl -O https://raw.githubusercontent.com/netmanito/repo/master/bash-aliases-functions
         echo ".bash_aliases found on your home directory"
         echo "adding data to it ..."
         echo "## bash aliases extra " >> ~/.bash_aliases
         cat bash-aliases-extra >> .bash_aliases
+        cat bash-aliases-functions >> .bash_aliases
         echo "updating shell"
         source .bashrc
-        rm bash-aliases-extra
+        rm bash-aliases-{extra,functions}
         echo "All Done!!"
 
 fi
